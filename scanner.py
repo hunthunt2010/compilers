393b13c7eb778816a6144daf3f338e870832ba78
Ezekiel <echopper@mines.eu>
2014-04-11 00:05:48 -0600

    fixing the file

--- scanner.py
+++ scanner.py
@@ -1,30 +1,96 @@
-tokens = (
-    'IDENTIFIER', 'CLOSECURLYBRACE', 'CLOSEPAREN', 'COMMA', 
-    'CONST', 'CONSTANTINTEGER', 'DIVIDE', 'ELSE', 'EQUALS', 
-    'IF', 'LEFTSHIFT', 'LESSTHANOREQUAL', 'MINUS', 'MULTIPLY', 
-    'OPENCURLYBRACE', 'OPENPAREN', 'PLUS', 'RETURN', 'SEMICOLON', 
-    'TYPEINTEGER'
-    )
+#!/usr/bin/env python
 
+# const int a = 1;
+# int x = 1<<a;
+# int y, z = 3;
+# 
+# y = z - x;
+# if ( y <= 0 ) {
+#    z = (x+2) + z*z ;
+# } else {
+#    z = z / y;
+# }
+# 
+# return z;
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+reserved = {'int' : 'int',
+        'const' : 'const',
+        }
+
+tokens = [
+        'semi', 
+        'identifier',
+    ] + list(reserved.values())
+
+t_semi = r';'
 t_ignore = " \t\n"
 
-t_IDENTIFIER = r'[a-zA-Z]+[a-zA-Z0-9]*'
-t_CLOSECURLYBRACE = r'\}'
-t_CLOSEPAREN  = r'\)'
-t_COMMA = r','
-t_CONST	= r'const'
-t_CONSTANTINTEGER = r'[1-9][0-9]*'
-t_DIVIDE = r'/'
-t_ELSE	= r'else'
-t_EQUALS = r'='
-t_IF = r'if'
-t_LEFTSHIFT = r'<<'
-t_LESSTHANOREQUAL = r'<='
-t_MINUS	= r'-'
-t_MULTIPLY = r'\*'
-t_OPENCURLYBRACE = r'\{'
-t_OPENPAREN = r'\('
-t_PLUS = r'\+'
-t_RETURN = r'return'
-t_SEMICOLON = r';'
-t_TYPEINTEGER = r'int'
+def t_identifier(t):
+    r'[a-zA-Z_]+[a-zA-Z0-9_]*'
+    if t.value in reserved:
+        t.type = reserved[ t.value ]
+    return t
+
+def t_error(t):
+    print("Illegal character :" + str(t.value))
+    t.lexer.skip(1)
+
+def p_START(p):
+    '''START : STMT'''
+    #print("START" + p[0])
+    return
+
+def p_STMT(p):
+    '''STMT : DECL semi'''
+    #print("STMT:" + p[1] + p[2])
+
+def p_DECL(p):
+    '''DECL : TYPE VARIABLE'''
+    #p[0] = p[1] + p[2]
+    #print("p[1]: <%s>" % p[1])
+    #print("p[2]: <%s>" % p[2])
+    #print("DECL:" + p[0])
+
+def p_VARIABLE(p):
+    '''VARIABLE : identifier'''
+    #p[0] = ["id", p[1]]
+
+def p_TYPE(p):
+    '''TYPE : MODIFIER int'''
+    #p[0] = p[1] + p[2]
+    #print("TYPE:" + p[0])
+
+def p_TYPE_INT(p):
+    '''TYPE : int'''
+    #p[0] = p[1]
+    print("TYPE: %s" % p)
+
+def p_MODIFIER(p):
+    'MODIFIER : const'
+    p[0] = ('MODIFIER', p[1])
+    #p[0] = p[1]
+    #print("MOD:" + p[0])
+
+def p_error(p):
+    print("yacc error: %s" % p)
+
+lex.lex()
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('Validate String > ')
+    except EOFError:
+        break
+    if s == 'quit':
+        break
+    lex.input(s)
+    while True:
+        tok = lex.token()
+        if not tok: break
+        print(tok)
+
+    print("Result returned: " + str(parser.parse(s)))
