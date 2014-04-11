3f2ada69fb7388540386d50f151f2c559ee938ef
ccopper <ccopper@mymail.mines.edu>
2014-04-11 15:15:18 -0600

    Added Nodes to P functions

--- ToTheAST/scanner.py
+++ ToTheAST/scanner.py
@@ -13,10 +13,12 @@
 # 
 # return z;
 
+import re
+
 import ply.lex as lex
 import ply.yacc as yacc
 
-import node
+from node import Node
 
 reserved = {'int' : 'int',
         'const' : 'const',
@@ -64,7 +66,8 @@ def t_identifier(t):
 
 def t_int_value(t):
     r'[0-9][0-9]*'
-    return int(t.value)
+    t.value = int(t.value)
+    return t
 
 def t_error(t):
     print("Illegal character :" + str(t.value))
@@ -72,59 +75,101 @@ def t_error(t):
 
 def p_START(p):
     '''START : STMTS'''
+    p[0] = Node("START", "START")
+    p[0].addChild(p[1])
 
 def p_STMTS(p):
     'STMTS : STMT STMTS'
-    p[0] = ('STATEMENT', p[1])   #This could be a problem
+    p[0] = Node("STMTS",  "STMTS")
+    p[0].addChild(p[1])
+    p[0].addChild(p[2])
 
 def p_EMPTY_STMTS(p):
     'STMTS :'
 
 def p_STMT_DECL(p):
     '''STMT : DECL semi'''
+    p[0] = p[1]
 
 def p_STMT_EXPR(p):
     'STMT : EXPR semi'
+    p[0] = p[1]
 
 def p_STMT_RETURN(p):
     'STMT : RETURN semi'
+    p[0] = p[1]
 
 def p_STMT_ASSIGN(p):
-    'STMT : identifier assign EXPR'
+    'STMT : identifier assign EXPR semi'
+    p[0] = Node("ASSIGN", "ASSIGN")
+    p[0].addChild(p[3])
 
 def p_IF_STMT(p):
     'STMT : if lp EXPR rp CODEBLOCK'
+    p[0] = Node("IF", "IF")
+    p[0].addChild(p[3])
+    p[0].addChild(p[5])
+
 
 def p_IF_ELSE_STMT(p):
     'STMT : if lp EXPR rp CODEBLOCK else CODEBLOCK'
+    p[0] = Node("IF_ELSE" ,"IF_ELSE")
+    p[0].addChild(p[3])
+    p[0].addChild(p[5])
+    p[0].addChild(p[7])
+
 
 def p_DECL(p):
     '''DECL : TYPE VARIABLE MULTI'''
+    p[0] = Node("DECL", "DECL")
+    p[0].addChild(p[1])
+    p[0].addChild(p[2])
+    p[0].addChild(p[3])
 
 def p_DECL_ASSIGN(p):
     '''DECL : TYPE VARIABLE assign EXPR MULTI'''
-    
+    p[0] = Node("DECL", "DECL")
+    p[0].addChild(p[1])
+    p[0].addChild(p[2])
+    p[0].addChild(p[4])
+    p[0].addChild(p[5])
+
 def p_MULTI(p):
-    '''MULTI : comma VARIABLE'''
+    '''MULTI : comma VARIABLE MULTI'''
+    p[0] = Node("MULTI" , "MULTI" )
+    p[0].addChild(p[2])
+    p[0].addChild(p[3])
 
 def p_MULTI_ASSIGN(p):
-    '''MULTI : comma VARIABLE assign EXPR'''
+    '''MULTI : comma VARIABLE assign EXPR MULTI'''
+    p[0] = Node("MULTI_ASSIGN" , "MULTI_ASSIGN" )
+    p[0].addChild(p[2])
+    p[0].addChild(p[4])
+    p[0].addChild(p[5])
 
 def p_MULTI_LAMBDA(p):
     '''MULTI : '''
 
 def p_VARIABLE(p):
     '''VARIABLE : identifier'''
-    p[0] = ('VARIABLE', p[1])
+    p[0] = Node("VARIABLE", p[1])
 
 def p_EXPR_ORDER(p):
     'EXPR : lp EXPR rp'
+    p[0] = Node("EXPR", "EXPR")
+    p[0].addChild(p[2])
 
 def p_EXPR_BINOP(p):
     'EXPR : EXPR BINARYOPERATOR EXPR'
+    p[0] = Node("EXPR_BINOP", "EXPR_BINOP")
+    p[0].addChild(p[1])
+    p[0].addChild(p[2])
+    p[0].addChild(p[3])
 
 def p_EXPR_VALUE(p):
     'EXPR : VALUE'
+    p[0] = Node("EXPR", "VALUE")
+    p[0].addChild(p[1])
 
 def p_BINARYOPERATOR(p):
     '''BINARYOPERATOR : leftshift
@@ -133,32 +178,40 @@ def p_BINARYOPERATOR(p):
                       | multiply
                       | divide
                       | lessequal'''
-    p[0] = ('BINARYOPERATOR', p[1])
+    p[0] = Node("BINARYOPEAROR", p[1])
 
 def p_RETURN(p):
     'RETURN : return'
+    p[0] = Node("RETURN", "RETURN")
 
 def p_RETURN_EXPR(p):
-    'RETURN : EXPR'
+    'RETURN : return EXPR'
+    p[0] = Node("RETURN", "RETURN")
+    p[0].addChild(p[2])
 
 def p_CODEBLOCK(p):
     'CODEBLOCK : lb STMTS rb'
+    p[0] = Node("CODEBLOCK", "CODEBLOCK")
+    p[0].addChild(p[2])
 
 def p_VALUE(p):
     '''VALUE : int_value
              | identifier'''
+    p[0] = Node("VALUE", p[1])
 
 def p_TYPE(p):
     '''TYPE : MODIFIER TYPE'''
-    p[0] = ('TYPE', "%s %s" % (p[1], p[2]))
+    p[0] = Node('TYPE',"TYPE")
+    p[0].addChild(p[1])
+    p[0].addChild(p[2])
 
 def p_TYPE_INT(p):
     '''TYPE : int'''
-    p[0] = ('TYPE', p[1])
+    p[0] = Node('TYPE', p[1])
                           
 def p_MODIFIER(p):
     'MODIFIER : const'
-    p[0] = ('MODIFIER', p[1])
+    p[0] = Node("MODIFIER","const")
 
 def p_error(p):
     print("yacc error: %s" % p)
@@ -166,11 +219,9 @@ def p_error(p):
 lex.lex()
 parser = yacc.yacc()
 
-while True:
-    try:
-        s = input()
-    except EOFError:
-        break
+
+s = open("sample.txt", 'r').read()
+
     #if s == 'quit':
     #    break
 re.sub(r'//.*$', '', s)
@@ -180,4 +231,9 @@ while True:
     if not tok: break
     print(tok)
 
-    print("Result returned: " + str(parser.parse(s)))
+root = parser.parse(s)
+
+print(root.getNames())
+print(root.getChildren())
+
+
